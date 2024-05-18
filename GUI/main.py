55ac75b5eba69cd8eaa61b343223de99340ffb94
s4661768 <nemandy.02@gmail.com>
2024-05-18 14:18:28 +1000

    Added a way for the user the define which COM port the program connects to and decide which baud rate to use

--- GUI/main.py
+++ GUI/main.py
@@ -5,6 +5,7 @@ data using the MQTT class. """
 
 
 import threading
+import serial
 
 from gui import  GUI
 from gesture_recogniser import Gesture_Recogniser
@@ -12,16 +13,39 @@ from mqtt import MQTT
 from thingy52 import Thingy52_Receiver
 
 def main():
+    print("Starting...")
+    com_baud = input("Enter COM port and baud rate (e.g. COM11 115200): ")
+    try:
+        com_port, baud_rate = com_baud.split(" ")
+        baud = int(baud_rate)
+        if baud < 9600:
+            raise ValueError("Invalid baud rate: Too low")
+        print(com_port, baud_rate)
+        # self.ser = serial.Serial(com_port, baud_rate)
+    except ValueError:
+        print("Invalid input")
+        exit(1)
+
+    try:
+        # Testing serial port
+        ser = serial.Serial(com_port, baud_rate)
+        ser.close()
+    except serial.SerialException as e:    
+        print("Error connecting to serial port:", e)
+        exit(1)
+
+    """ If the serial port opened successfully we can continue with the rest of the program. """
+
     mqtt = MQTT() # Make MQTT object
     
-    # Make the start the Thingy52_Receiver in a separate thread
-    threading.Thread(target=Thingy52_Receiver, args=(mqtt, "COM11", 115200,), daemon=True).start()
+    # # Make the start the Thingy52_Receiver in a separate thread
+    threading.Thread(target=Thingy52_Receiver, args=(mqtt, com_port, baud,), daemon=True).start()
     
-    # Make the start the GUI in a separate thread
+    # # Make the start the GUI in a separate thread
     threading.Thread(target=GUI, args=(mqtt,), daemon=True).start()
 
-    # Make the start the Gesture_Recogniser in a separate thread
-    # Must by called in main thread to use the webcam properly
+    # # Make the start the Gesture_Recogniser in a separate thread
+    # # Must by called in main thread to use the webcam properly
     gr = Gesture_Recogniser(mqtt)
     
 
